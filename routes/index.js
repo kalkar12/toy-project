7317d7a32b670acca350a26e060ffde7d8ba9749
kalkar12 <78849506+kalkar12@users.noreply.github.com>
2021-03-01 17:26:36 +0200

    Delete index.js

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.get("/", (req,res) => {
-  res.json({msg:"index work! 22222 3333"});
-})
-
-module.exports = router;
